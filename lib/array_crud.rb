5ffc4ffb487f8b46e0c8731d5ba6462d438c3a2d
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-06-26 05:57:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,6 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   my_array = ["hi","bye","hey","see ya"]
   my_array << "howdy"
+  p my_array
 end
 
 def add_element_to_start_of_array(array, element)

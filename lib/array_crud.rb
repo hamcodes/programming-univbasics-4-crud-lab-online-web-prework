afc658f5cf976feecaeba4e0704af055860bd944
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-06-26 06:17:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   my_array = ["wow","hi","bye","hey","see ya","arrays!"]
-  ("arrays!") = my_array.pop
+  arrays! = my_array.pop
 end
 
 def remove_element_from_start_of_array(array)

3ecc41b37668fcbffbeb6910a7060982a4c2ac2e
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-06-26 05:44:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,9 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  fruit = ["apple","orange","kiwi","blueberry"
-  fruit << "strawberry"
-  p fruit
+
 end
 
 def add_element_to_start_of_array(array, element)
